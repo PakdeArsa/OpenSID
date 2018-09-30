8aabd3fe24e1377e78d0761cab95b367dfd67866
Eddie Ridwan <eddie.ridwan@gmail.com>
2018-09-30 20:16:19 +1000

    Pindahkan komponen surat_ket_pindah_penduduk keluar dari Surat_model.php

--- donjo-app/controllers/Surat.php
+++ donjo-app/controllers/Surat.php
@@ -188,7 +188,6 @@ class Surat extends CI_Controller {
 		$data['penduduk'] = $this->surat_model->list_penduduk();
 		$data['pamong'] = $this->surat_model->list_pamong();
 		$data['perempuan'] = $this->surat_model->list_penduduk_perempuan();
-		$data['kode'] = $this->surat_model->get_daftar_kode_surat($url);
 
 		$data_form = $this->surat_model->get_data_form($url);
 		if (is_file($data_form))
--- donjo-app/models/Surat_model.php
+++ donjo-app/models/Surat_model.php
@@ -503,59 +503,6 @@
 		}
 	}
 
-	public function get_daftar_kode_surat($surat)
-	 {
-		$kode = array();
-		switch ($surat)
-		{
-			case 'surat_ket_pindah_penduduk':
-				$kode["alasan_pindah"] = array(
-					1 => "Pekerjaan",
-					2 => "Pendidikan",
-					3 => "Keamanan",
-					4 => "Kesehatan",
-					5 => "Perumahan",
-					6 => "Keluarga",
-					7 => "Lainnya"
-				);
-				$kode["klasifikasi_pindah"] = array(
-					1 => "Dalam satu Desa/Kelurahan",
-					2 => "Antar Desa/Kelurahan",
-					3 => "Antar Kecamatan",
-					4 => "Antar Kab/Kota",
-					5 => "Antar Provinsi"
-				);
-				$kode["jenis_kepindahan"] = array(
-					1 => "Kep. Keluarga",
-					2 => "Kep. Keluarga dan Seluruh Angg. Keluarga",
-					3 => "Kep. Keluarga dan Sbg. Angg. Keluarga",
-					4 => "Angg. Keluarga"
-				);
-				$kode["status_kk_pindah"] = array(
-					1 => "Numpang KK",
-					2 => "Membuat KK Baru",
-					3 => "Nomor KK Tetap"
-				);
-				$kode["status_kk_tidak_pindah_f108"] = array(
-					1 => "Numpang KK",
-					2 => "Membuat KK Baru",
-					3 => "Tidak Ada Angg. Keluarga Yang Ditinggal",
-					4 => "Nomor KK Tetap"
-				);
-				$kode["status_kk_tidak_pindah"] = array(
-					1 => "Numpang KK",
-					2 => "Membuat KK Baru",
-					3 => "Nomor KK Tetap"
-				);
-				break;
-
-			default:
-				# code...
-				break;
-		}
-		return $kode;
-	}
-
 	// Untuk surat sistem, cek apakah komponen surat sudah disesuaikan oleh desa
 	private function lokasi_komponen($nama_surat, $komponen)
 	{
